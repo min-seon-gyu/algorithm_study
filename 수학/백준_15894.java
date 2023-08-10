8b25abcf3d62292e076a3d73bc65fe65a89fe7df
sseung416 <choiseungyeon416@gmail.com>
2023-08-10 16:32:07 +0900

    푼 문제 백업(수학, 카카오 기출)

---
+++ 수학/백준_15894.java
@@ -0,0 +1,11 @@
+package codingtest;
+
+import java.io.*;
+
+public class 백준_15894 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(Long.parseLong(br.readLine()) * 4);
+        br.close();
+    }
+}
---
+++ 수학/백준_6064_2.java
@@ -0,0 +1,34 @@
+package codingtest;
+
+import java.util.*;
+import java.io.*;
+
+// x를 기준으로 y값을 지정함
+public class 백준_6064_2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int m = Integer.parseInt(st.nextToken());
+            int n = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            int result = -1;
+            int tempY = x;
+            for (int k = x; k <= m * n; k += m) {
+                tempY = tempY % n == 0 ? n : tempY % n;
+                if (tempY == y) {
+                    result = k;
+                    break;
+                }
+                tempY += m;
+            }
+            sb.append(result).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
+
